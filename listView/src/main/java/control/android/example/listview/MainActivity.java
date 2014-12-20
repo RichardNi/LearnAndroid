dbd51ee3ed50873c61124dc37637834a57883215
RichardNi <richardni@163.com>
2014-12-20 13:53:27 +0800

    添加ListView SimpleAdapter示例

--- listView/src/main/java/control/android/example/listview/MainActivity.java
+++ listView/src/main/java/control/android/example/listview/MainActivity.java
@@ -4,11 +4,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import control.android.example.R;
 
+
 /**
- * ListView ArrayAdapter示例
+ * 演示ListView的ArrayAdapter、SimpleAdapter
  *
  * @author RichardNi
  */
@@ -27,7 +34,31 @@ public class MainActivity extends ActionBarActivity {
 
         ListView listView = (ListView) findViewById(R.id.lv_show);
         // 设置adapter到ListView上
-        listView.setAdapter(arrayAdapter);
+//        listView.setAdapter(arrayAdapter);
+
+        /**
+         * data:数据源
+         * resource:ListView列表项的布局文件
+         * from:数据源中Map对象的键(Key)列表
+         * to:保存布局文件中控件的id列表(与前面的from里面保存的键列表一一对应)
+         **/
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, getData(), R.layout.item,
+                new String[]{"picture", "content"}, new int[]{R.id.pic, R.id.txt});
+
+        listView.setAdapter(simpleAdapter);
+
     }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> maps = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("content", "不错哦，哈哈" + i);
+            map.put("picture", R.drawable.ic_launcher);
+            maps.add(map);
+        }
+        return maps;
+    }
+
 
 }
