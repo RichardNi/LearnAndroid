8cd76a21bd1a4fbcaeffb725b4e115216c229060
RichardNi <richardni@163.com>
2014-12-20 14:52:54 +0800

    添加ListView滚动事件

--- listView/src/main/java/control/android/example/listview/MainActivity.java
+++ listView/src/main/java/control/android/example/listview/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -25,9 +26,11 @@ import control.android.example.R;
  */
 public class MainActivity extends ActionBarActivity {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "Main";
 
     private ListView listView;
+    private List<Map<String, Object>> dataList;
+    private SimpleAdapter simpleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +53,12 @@ public class MainActivity extends ActionBarActivity {
          * from:数据源中Map对象的键(Key)列表
          * to:保存布局文件中控件的id列表(与前面的from里面保存的键列表一一对应)
          **/
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, getData(), R.layout.item,
+        simpleAdapter = new SimpleAdapter(this, getData(), R.layout.item,
                 new String[]{"picture", "content"}, new int[]{R.id.pic, R.id.txt});
 
         listView.setAdapter(simpleAdapter);
         listView.setOnItemClickListener(onItemClickListener);
+        listView.setOnScrollListener(onScrollListener);
 
     }
 
@@ -72,16 +76,49 @@ public class MainActivity extends ActionBarActivity {
         }
     };
 
+    /**ListView滚动事件*/
+    private AbsListView.OnScrollListener onScrollListener = new AbsListView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+            switch (scrollState) {
+                case SCROLL_STATE_FLING:
+                    Log.i(TAG, "用户手指在离开屏幕之前，由于用力滑了一下，视图由于惯性仍在滑动");
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("content", "新增出来的数据哦");
+                    map.put("picture", R.drawable.ic_launcher);
+                    // 添加新的数据到数据源中
+                    dataList.add(map);
+                    // 通知adapter数据改变，刷新UI
+                    simpleAdapter.notifyDataSetChanged();
+                    break;
+                case SCROLL_STATE_IDLE:
+                    Log.i(TAG, "视图已经停止滚动");
+                    break;
+                case SCROLL_STATE_TOUCH_SCROLL:
+                    Log.i(TAG, "手指没有离开屏幕，视图正在滑动");
+                    break;
+                default:
+                    Log.i(TAG, "未知状态：" + scrollState);
+                    break;
+            }
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+        }
+    };
+
     private List<Map<String, Object>> getData() {
-        List<Map<String, Object>> maps = new ArrayList<>();
+        dataList = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             Map<String, Object> map = new HashMap<>();
             map.put("content", "不错哦，哈哈" + i);
             map.put("picture", R.drawable.ic_launcher);
             map.put("id", i);
-            maps.add(map);
+            dataList.add(map);
         }
-        return maps;
+        return dataList;
     }
 
 
