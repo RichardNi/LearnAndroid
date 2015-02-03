65eed048073405108127c67179bd7b856fcb19d3
RichardNi <richardni@163.com>
2015-02-03 12:52:53 +0800

    增加通过ImageSwitcher显示图片的代码

--- gallery-demo/src/main/java/com/example/richard/gallery/MainActivity.java
+++ gallery-demo/src/main/java/com/example/richard/gallery/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.richard.gallery;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.Gallery;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener,
+        ViewSwitcher.ViewFactory {
+
+
+    private static final String TAG = "MainActivity";
 
     private int[] resourceImageIds = {R.drawable.item1, R.drawable.item2, R.drawable.item3,
             R.drawable.item4, R.drawable.item5, R.drawable.item6, R.drawable.item7,
@@ -15,6 +25,7 @@ public class MainActivity extends ActionBarActivity {
             R.drawable.item12};
 
     private Gallery gallery;
+    private ImageSwitcher imageSwitcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +34,34 @@ public class MainActivity extends ActionBarActivity {
         gallery = (Gallery) findViewById(R.id.gallery);
         MyImageAdapter adapter = new MyImageAdapter(resourceImageIds, this);
         gallery.setAdapter(adapter);
+        gallery.setOnItemSelectedListener(this);
+
+        imageSwitcher = (ImageSwitcher) findViewById(R.id.imageSwitcher);
+        imageSwitcher.setFactory(this);
+        imageSwitcher.setInAnimation(this, android.R.anim.fade_in);
+        imageSwitcher.setOutAnimation(this, android.R.anim.fade_out);
+//        imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+//        imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, "position=" + position + ", id=" + id);
+        imageSwitcher.setBackgroundResource(resourceImageIds[(int) id]);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public View makeView() {
+        ImageView imageView = new ImageView(this);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+        return imageView;
+    }
 }
